8ce10e942abfb6dda62ebf95e12c1a23c88d3503
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-22 11:44:34 -0500

    Librerias importadas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vSelect from "vue-select";
+import VueTableLite from "vue3-table-lite";
+import Toast from "vue-toastification";
 
-createApp(App).use(router).mount('#app')
+// * import Pinia
+import { createPinia } from "pinia";
+
+//* import Bootstrap 5
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+//* import styles
+import "vue-select/dist/vue-select.css";
+import "vue-toastification/dist/index.css";
+
+const app = createApp(App);
+app.use(router);
+app.use(createPinia());
+app.use(Toast);
+app.component("v-select", vSelect);
+app.component("table-lite", VueTableLite);
+app.mount("#app");
