44d7a60347413df72695d1bf7f338fd3a25a1825
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-22 20:10:00 -0500

    Vista de dependencias, secciones y subsecciones terminada

---
+++ src/helpers/TableHeaders/Department.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        label: "ID",
+        field: "id",
+        width: "3%",
+        sortable: true,
+        isKey: true,
+    },
+    {
+        label: "Nombre",
+        field: "nombre",
+        width: "10%",
+        sortable: true,
+    },
+    {
+        label: "Sección",
+        field: "identificador_interno",
+        width: "10%",
+        sortable: true,
+    },
+];
--- src/views/office/OfficeInformation.vue
+++ src/views/office/OfficeInformation.vue
@@ -1,5 +1,20 @@
 <template>
-    <office-form :readonly="true" :object="office" />
+    <div class="container-information">
+        <office-form :readonly="true" :object="office" />
+        <div class="container-relations">
+            <custom-table
+                @row-selected="selectDepartment"
+                :columns="departmentHeaders"
+                :rows="departments"
+                :quantity-show="5"
+            />
+            <custom-table
+                :columns="subdepartmentHeaders"
+                :rows="subdepartments"
+                :quantity-show="5"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,21 +22,45 @@ import { defineAsyncComponent } from "@vue/runtime-core";
 import { mapStores } from "pinia";
 import { useOfficeStore } from "@/stores/office/OfficeStore";
 import { useToast } from "vue-toastification";
+import departmentColumns from "@/helpers/TableHeaders/Department";
+import subdepartmentColumns from "@/helpers/TableHeaders/Subdepartment";
 const toast = useToast();
 export default {
     components: {
         OfficeForm: defineAsyncComponent(() =>
             import("@/components/office/OfficeForm.vue")
         ),
+        CustomTable: defineAsyncComponent(() =>
+            import("@/components/Table.vue")
+        ),
     },
     props: {
         id: Number,
+    },
+    data() {
+        return {
+            subdepartments: [],
+        }
     },
     computed: {
         ...mapStores(useOfficeStore),
         office() {
             return this.officeStore.finded;
         },
+        departmentHeaders() {
+            return departmentColumns;
+        },
+        subdepartmentHeaders() {
+            return subdepartmentColumns;
+        },
+        departments() {
+            return this.officeStore.finded.departments;
+        },
+    },
+    methods: {
+        selectDepartment(department) {
+            this.subdepartments = department.subdepartments;
+        }
     },
     async mounted() {
         await this.officeStore.find(this.id);
@@ -29,4 +68,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.container-information {
+    display: grid;
+    grid-template-columns: 4fr 6fr;
+}
+.container-relations {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+}
+</style>
