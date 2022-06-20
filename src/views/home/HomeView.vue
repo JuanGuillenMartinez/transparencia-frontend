e561202fc0254b81d34620c7d1583ffb0a21c73b
JuanFlP30 <zapata_pipe@hotmail.com>
2022-06-20 11:51:29 -0500

    Cambios en el index

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
+    <!DOCTYPE html>
+<html class="container-html" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transparencia</title>
+</head>
+
+<body class=".contenido-body">
     <header>
-        <img class="logo" src="img/logo4.jpg" alt="" />
-        <h1 class="titulo">
-            Coordinación de Transparencia y Acceso a la Información
-        </h1>
+        <img class="logo" src="img/logo4.jpg" alt="">
+        <h1 class="titulo">Coordinación de Transparencia y Acceso a la Información</h1>
     </header>
 
     <div class="nav-bg">
@@ -15,47 +24,25 @@
         </nav>
     </div>
 
+
     <section class="hero">
         <div class="contenido-hero">
             <h2>Sistema de Prestamo de Archivos</h2>
             <p>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="icon icon-tabler icon-tabler-map-pin"
-                    width="80"
-                    height="80"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="#FFC107"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="80"
+                    height="80" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFC107" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <circle cx="12" cy="11" r="3" />
-                    <path
-                        d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"
-                    />
-                </svg>
-                Villahermosa, Tabasco
+                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                </svg> Villahermosa, Tabasco
             </p>
 
             <a class="boton" href="#">Contactar</a>
             <div>
                 <a href="https://www.villahermosa.gob.mx/">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="icon icon-tabler icon-tabler-building-bank"
-                        width="40"
-                        height="40"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="#ffffff"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-bank" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <line x1="3" y1="21" x2="21" y2="21" />
                         <line x1="3" y1="10" x2="21" y2="10" />
                         <polyline points="5 6 12 3 19 6" />
@@ -64,14 +51,24 @@
                         <line x1="8" y1="14" x2="8" y2="17" />
                         <line x1="12" y1="14" x2="12" y2="17" />
                         <line x1="16" y1="14" x2="16" y2="17" />
-                    </svg>
+                      </svg>
                 </a>
             </div>
+
         </div>
+
     </section>
-    <footer class="footer">
-        <p>Todos los derechos reservador.</p>
-    </footer>
+
+
+        <footer class="footer">
+            <p>Todos los derechos reservador.</p>
+        </footer>
+
+
+
+</body>
+
+</html>
 </template>
 
 <script>
@@ -79,65 +76,51 @@ export default {};
 </script>
 
 <style scoped>
-:root {
-    --primario: #ffc107;
-    --secundario: #0097a7;
+:root{
+    --primario: #ffc107; 
+    --secundario: #0097A7;
     --blanco: #ffffff;
     --oscuro: #000;
-    --gris: #757575;
+    --gris: #757575; 
 }
-html {
+.container-html {
     font-size: 62.5%;
     box-sizing: border-box;
 }
-*,
-*:before,
-*:after {
-    box-sizing: inherit;
-}
-body {
+.contenido-body{
     font-size: 16px; /*1rem = 10px*/
-    font-family: "Krub", sans-serif;
-    background-image: linear-gradient(to top, #dfe9f3 0%, var(--blanco) 100%);
+    font-family: 'Krub', sans-serif;
+    background-image: linear-gradient(to top, #DFE9F3 0%, var(--blanco) 100%);
 }
 
-h1 {
-    font-size: 2.8rem;
+
+.logo{
+   width: 25rem;
+   height: 8.7rem;
+   margin: 2rem; 
+   position: absolute;
 }
-h2 {
-    font-size: 2.8rem;
-}
-h3 {
-    font-size: 1.8rem;
-}
-h1,
-h2,
-h3 {
+.titulo{
+    font-size: 3.3rem;
+    font-family: 'Krub', sans-serif;
     text-align: center;
 }
 
-.logo {
-    width: 25rem;
-    height: 8.7rem;
-    margin-left: 15rem;
-    margin-bottom: -6.7rem;
-}
-
-.contenedor {
+.contenedor{
     max-width: 120rem;
-    margin: 0 auto;
+    margin-top: 8rem;
 }
 
-.sombra {
-    -webkit-box-shadow: 0px 5px 15px 0px rgba(140, 137, 140, 1);
-    -moz-box-shadow: 0px 5px 15px 0px rgba(140, 137, 140, 1);
-    box-shadow: 0px 5px 15px 0px rgba(140, 137, 140, 1);
+.sombra{
+    -webkit-box-shadow: 0px 5px 15px 0px rgba(140,137,140,1);
+    -moz-box-shadow: 0px 5px 15px 0px rgba(140,137,140,1);
+    box-shadow: 0px 5px 15px 0px rgba(140,137,140,1);
     background-color: var(--blanco);
     padding: 2rem;
     border-radius: 1rem;
 }
 
-.boton {
+.boton{
     background-color: #831919;
     color: var(--blanco);
     padding: 1rem 3rem;
@@ -146,58 +129,57 @@ h3 {
     text-decoration: none;
     text-transform: uppercase;
     font-weight: bold;
-    border-radius: 0.5rem;
+    border-radius: .5rem;
     width: 90%;
     text-align: center;
     border: none;
 }
 
 @media (min-width: 768px) {
-    .boton {
+    .boton{
         width: auto;
     }
 }
 
-.boton,
-.buton:hover {
-    cursor: pointer;
+.boton,.buton:hover{
+cursor: pointer;
 }
 
-.w-sm-100 {
+.w-sm-100{
     width: 100%;
 }
 
 @media (min-width: 768px) {
-    .w-sm-100 {
+    .w-sm-100{
         width: auto;
     }
 }
 
-.flex {
+.flex{
     display: flex;
 }
 
-.alinearr-derecha {
+.alinearr-derecha{
     justify-content: flex-end;
 }
 
-.nav-bg {
+.nav-bg{
     background-color: #831919;
 }
 
-.navegacion-principal {
+.navegacion-principal{
     display: flex;
     flex-direction: column;
-}
+   }
 
 @media (min-width: 768px) {
-    .navegacion-principal {
+    .navegacion-principal{
         flex-direction: row;
         justify-content: space-between;
-    }
+    }    
 }
 
-.navegacion-principal a {
+.navegacion-principal a{
     display: block;
     text-align: center;
     color: var(--blanco);
@@ -207,13 +189,12 @@ h3 {
     padding: 1rem;
 }
 
-.navegacion-principal a:hover {
+.navegacion-principal a:hover{
     background-color: var(--primario);
     color: var(--oscuro);
 }
 
-.hero {
-    /* background-image: url(../img/hero.jpg); */
+.hero{ 
     background-repeat: no-repeat;
     background-size: cover;
     height: 410px;
@@ -221,7 +202,7 @@ h3 {
     margin-bottom: 2rem;
 }
 
-.contenido-hero {
+.contenido-hero{
     position: absolute;
     background-color: rgb(0 0 0 / 70%);
     width: 100%;
@@ -233,36 +214,36 @@ h3 {
     justify-content: center;
 }
 
-.contenido-hero h2,
-.contenido-hero p {
+.contenido-hero h2, .contenido-hero p{
     color: var(--blanco);
 }
 
-@media (min-width: 768px) {
-    .servicios {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        column-gap: 1rem;
+@media (min-width: 768px){
+    .servicios{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 1rem;
+    
     }
 }
 
-.servicio {
+.servicio{
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
-.servicio h3 {
+.servicio h3{
     color: var(--secundario);
     font-weight: normal;
 }
 
-.servicio p {
+.servicio p{
     line-height: 2;
     text-align: center;
 }
 
-.servicio .iconos {
+.servicio .iconos{
     height: 15rem;
     width: 15rem;
     background-color: var(--primario);
@@ -272,12 +253,12 @@ h3 {
     align-items: center;
 }
 
-.proyect {
+.proyect{
     height: 70rem;
     width: 70rem;
 }
 
-.formulario {
+.formulario{
     background-color: var(--gris);
     width: min(60rem, 100%);
     margin: 0 auto;
@@ -285,11 +266,11 @@ h3 {
     border-radius: 1rem;
 }
 
-.formulario fieldset {
-    border: none;
+.formulario fieldset{
+    border:none;
 }
 
-.formulario legend {
+.formulario legend{
     text-align: center;
     font-size: 1.8rem;
     transform: uppercase;
@@ -298,43 +279,43 @@ h3 {
     color: var(--primario);
 }
 
-.contenedor-campos {
+.contenedor-campos{
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: auto auto 20rem;
     column-gap: 1rem;
 }
 
-.campo {
+.campo{
     margin-bottom: 1rem;
 }
 
-@media (min-width: 768px) {
-    .campo label {
-        color: var(--blanco);
-        font-weight: bold;
-        margin-bottom: 0.5rem;
-        display: block;
+@media (min-width: 768px ){
+    .campo label{
+    color: var(--blanco);
+    font-weight: bold;
+    margin-bottom: .5rem ;
+    display: block;   
     }
 
-    .campo:nth-child(3),
-    .campo:nth-child(4) {
+    .campo:nth-child(3),.campo:nth-child(4){
         grid-column: 1 / 3;
     }
 }
 
-.campo textarea {
-    height: 20rem;
+
+.campo textarea{
+ height: 20rem;
 }
 
-.input-text {
+.input-text{
     width: 100%;
     border: none;
     padding: 1.5rem;
-    border-radius: 0.5rem;
+    border-radius: .5rem;
 }
 
-.footer {
+.footer{
     text-align: center;
 }
 </style>
